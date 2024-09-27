8ba5a315ec997996fcf0bb8f6e2002a9967b1f86
CompSciHigh24 <csh-github@compscihigh.org>
2024-09-27 05:45:19 -0400

    Create input.js

---
+++ input.js
@@ -0,0 +1,20 @@
+// Uncomment this block to debug function input errors
+// function divide(num1, num2) {
+//     if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+//         return "Inputs must be numbers";
+//     }
+//     return num1 / num2;
+// }
+
+// console.log(divide(10, 2)); // Should return 5
+// console.log(divide(10, 'two')); // Input Error: "Inputs must be numbers"
+
+// function calculateArea(width, height) {
+//     if (width <= 0 || height <= 0) {
+//         return "Width and height must be positive numbers";
+//     }
+//     return width * height;
+// }
+
+// console.log(calculateArea(5, 10)); // Should return 50
+// console.log(calculateArea(-5, 10)); // Input Error: "Width and height must be positive numbers"
