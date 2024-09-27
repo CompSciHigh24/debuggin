ccbbc61503dcee45936a44e252d9614d4af904d2
CompSciHigh24 <csh-github@compscihigh.org>
2024-09-27 05:44:37 -0400

    Create reference.js

---
+++ reference.js
@@ -0,0 +1,20 @@
+// Uncomment this block to debug reference and scope errors
+// const name = "Debugging";
+
+// function greet() {
+//     console.log("Hello " + name);
+// }
+
+// greet(); // Should log: Hello Debugging
+
+// function outer() {
+//     let outerVar = "I'm outside";
+//     function inner() {
+//         let innerVar = "I'm inside";
+//         console.log(outerVar); // Should access outerVar correctly
+//     }
+//     inner();
+//     console.log(innerVar); // ReferenceError: innerVar is not defined
+// }
+
+// outer();
